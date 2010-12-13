e8ea5cc9841228b5fd53bc4e61f357ccf6cee265
Gabriel Le Roux <gabriel.le.roux@unboxedconsulting.com>
2010-12-13 12:23:14 +0200

    Log in done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GreenCape::Application.config.secret_token = 'c811594f6a946286a0be4dc1df03dda3f071d26a4e75289eefb2459e1ce346256b09559fa2f58c56bfeb9ef21904fb46e3de4a253fc8a4cb395531d3d4172963'
